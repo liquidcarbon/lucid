edc4cb69b0ba093b50a056c2ccd34b8c8e8ff82e
liquidcarbon <47034358+liquidcarbon@users.noreply.github.com>
2020-09-29 15:49:26 -0600

    Create viz.py

---
+++ viz.py
@@ -0,0 +1,165 @@
+# lucid/viz.py
+
+__doc__ = """
+Useful Bokeh Plots.  Each Bokeh plot is exposed as ``self.p`` class attribute
+and can be modified for final tweaks.
+"""
+
+#-----------------------------------------------------------------------------
+# Logging
+#-----------------------------------------------------------------------------
+import logging
+_l = logging.getLogger(__name__)
+
+
+#-----------------------------------------------------------------------------
+# Imports & Options
+#-----------------------------------------------------------------------------
+
+# Bokeh imports
+from bokeh.colors import RGB
+from bokeh.io import curdoc, output_file 
+from bokeh.models import ColorBar, LinearColorMapper
+from bokeh.models import ColumnDataSource, HoverTool, Label, LabelSet
+from bokeh.models import BasicTicker, PrintfTickFormatter, NumeralTickFormatter
+from bokeh.plotting import figure
+from bokeh.themes import Theme
+
+# External imports
+from math import pi
+from pylab import cm
+import numpy as np
+import pandas as pd
+
+# Lucid imports
+from .util import me
+
+
+#-----------------------------------------------------------------------------
+# Globals & Constants
+#-----------------------------------------------------------------------------
+
+theme1 = {'attrs': {
+    'Grid': {
+        'grid_line_color': None,
+    },
+
+    'Title': {
+        'text_font_size': '18pt',
+        'text_font': 'Segoe UI'
+    },
+
+    'Axis': {
+        'major_tick_in': -4,
+        'major_tick_out': 8,
+        'minor_tick_in': -2,
+        'minor_tick_out': 4,
+        'major_label_text_font_size': '14pt',
+        'axis_label_text_font_size': '14pt',
+        'axis_label_text_font': 'Segoe UI',
+    },
+    
+    'Legend': {
+        'background_fill_alpha': 0.4,
+    }
+}}
+curdoc().theme = Theme(json=theme1)
+
+# fill/hatch sequence for up to 20 categories
+fill_hatch = {
+    'fill': [
+        '#ff0000',
+        '#00ff00',
+        '#0000ff',
+        '#ffff00',
+        '#00ffff',
+        '#ff0000',
+        '#00ff00',
+        '#0000ff',
+        '#ffff00',
+        '#00ffff',
+        '#ff0000',
+        '#00ff00',
+        '#0000ff',
+        '#ffff00',
+        '#00ffff',
+        '#ff0000',
+        '#00ff00',
+        '#0000ff',
+        '#ffff00',
+        '#00ffff',
+    ],
+    'hatch': [
+        ' ',
+        ' ',
+        ' ',
+        ' ',
+        ' ',
+        ',',
+        ',',
+        ',',
+        ',',
+        ',',
+        'v',
+        'v',
+        'v',
+        'v',
+        'v',
+        'x',
+        'x',
+        'x',
+        'x',
+        'x',
+    ]
+}
+
+
+#-----------------------------------------------------------------------------
+# True-False Plot
+#-----------------------------------------------------------------------------
+
+class TrueFalsePlot:
+    """A chart for highlighting boolean relationships.
+    
+    Requires a stacked dataset with at least three columns:
+        ``['rows','cols','boolean']``
+    """
+
+    def __init__(self, df, plot=True, title='True-False Plot'):
+        colors = ['purple', 'gold']
+        mapper = LinearColorMapper(palette=colors, low=0, high=1)
+
+        p = figure(
+            title=title,
+            plot_width=600, plot_height=400,
+            x_range=df.cols.unique(),
+            y_range=df.rows.unique(),
+            x_axis_location='above',
+            y_axis_location='right',
+            toolbar_location=None,
+            tooltips=[('','@cols @ @rows')]
+        )
+
+        p.grid.grid_line_color = None
+        p.axis.axis_line_color = None
+        p.axis.major_tick_line_color = None
+        p.axis.major_label_standoff = 0
+        p.xaxis.major_label_standoff = 0
+        p.xaxis.major_label_text_font_size = '11px'
+        p.yaxis.major_label_text_font_size = '14px'
+        p.xaxis.major_label_orientation = 3.14 / 3
+
+        p.rect(
+            source=df,
+            x='cols',
+            y='rows',
+            width=1, height=1,
+            fill_color={'field': 'boolean', 'transform': mapper},
+            line_color='white', line_width=1,
+        )
+
+        self.p = p
+        if plot:
+            show(p)
+    return
+
